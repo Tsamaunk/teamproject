020bf281dbb6b0c548ee68eb73ebddc2706fe21d
Mike Gordo <mike@timegrabapp.com>
2013-03-05 20:50:19 -0500

    API doc update

--- api/doc.php
+++ api/doc.php
@@ -10,7 +10,8 @@
 <a href="#login">login</a>
 <a href="#signup">signup</a>
 <a href="#lost_password">lost_password</a></div>
-<h3>How do we check the session? (PHP CODE)</h3>
+<div style="background-color:#fd7;padding:10px;"><h3>How do we check the session? (PHP CODE)</h3>Have this code in the beggining of your file: 
+&lt;?php 
 include_once 'base.php';	// THIS THE THE BACKEND CONNECTOR
 $hlp = new Helper();		// CREATE THE HELPER
 
@@ -19,7 +20,7 @@ if (isset($_SESSION['userId']) && isset($_SESSION['userToken']) && $hlp->validTo
 	} else {
 		NO SESSION FOUND
 	}
-
+?&gt;</div> 
 <a name="login"></a>	
 <span style="background-color:#000;color:#fff;"><b>/api/?login</b></span>
 this function sets up the Session variable
@@ -30,6 +31,23 @@ RESPONSE:
 	{"success": true / false,
 	"error": error message}
 
+<div style="background-color:#f9d;padding:10px;"><h3>Example: (JQuery CODE)</h3>&lt;script&gt;
+	function login() {	
+		var email1 = $('#email').val();
+		var pass1 = $('#pass').val();
+		$.post('/api/?login', {email:email1, password:pass1}, function(data){
+			if (!data.success) {
+				// some error - display it. ideally we have to display some beautiful message right on the screen, not crazy popup
+				alert(data.error);
+			} else {
+				// user logged in - redirect
+				window.location = "index.php";
+			}
+		}); 	
+	}
+&lt;/script&gt;
+</div>
+	
 <a name="signup"></a>
 <span style="background-color:#000;color:#fff;"><b>/api/?signup</b></span>
 this function creates the user record and send the 'confirm email' email
