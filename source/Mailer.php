c2bf82acf38055e277d88761b9f7bed695ef9bec
root <root@LittleBoxOfHorrors.apogeenet.net>
2013-03-07 14:14:57 -0500

    Complete Mailer compostition function

--- source/Mailer.php
+++ source/Mailer.php
@@ -35,7 +35,6 @@
 					$this->subject = "Confirmation Notification";
 					$this->content = "Dear " . $ra1->firstName . ",<br><br>";
 					$this->content .= "Please confirm your email by clicking <a href='" . SITE_URL . "token.php?token=" . $token1->token . "'> here</a>.<br><br>";
-					$this->content .= "Thanks,<br>The ProjectSwitch Team";
 					break;
 				case 2:
 					$rd = $con->getUserById($token1->rdId);
@@ -44,7 +43,6 @@
 					$this->subject = "User Confirmation Notification";
 					$this->content = "Dear " . $rd->firstName . ",<br><br>";
 					$this->content .= "User " . $ra1->firstName . " " . $ra1->lastName . " needs confirmation: <a href='" . SITE_URL . "token.php?token=" . $token1->token . "'>CONFIRM</a>  <a href='" . SITE_URL . "token.php?token=" . $token2->token . ">DENY</a><br><br>";
-					$this->content .= "Thanks,<br>The ProjectSwitch Team";
 					break;
 				case 3:
 					$ra1 = $con->getUserById($token1->raId1);
@@ -52,7 +50,6 @@
 					$this->subject = "Confirmation Approved";
 					$this->content = "Dear " . $ra1->firstName . ",<br><br>";
 					$this->content .= "Congratulations, your email has been approved! Please login <a href='" . SITE_URL . "login.php" . "'>here</a>.<br><br>";
-					$this->content .= "Thanks,<br>The ProjectSwitch Team";
 					break;
 				case 4:
 					$ra1 = $con->getUserById($token1->raId1);
@@ -60,22 +57,57 @@
 					$this->address = $ra2->email;
 					$this->subject = "Switch Request Notification";
 					$this->content = "Dear " . $ra2->firstName . ",<br><br>";
-					$this->content = $ra1->firstName . " wants to switch a duty day with you. Please <a href='" . SITE_URL . "login.php" . "'>login</a> to respond to this request.<br><br>";
-					$this->content .= "Thanks,<br>The ProjectSwitch Team";
+					$this->content .= $ra1->firstName . " wants to switch a duty day with you: <a href='" . SITE_URL . "token.php?token=" . $token1->token . "'>ACCEPT</a>  <a href='" . SITE_URL . "token.php?token=" . $token2->token . ">DECLINE</a><br><br>"
+					$thic->content .= "<a href='" . SITE_URL . "login.php" . "'>Login</a> to view specifics.<br><br>";
 					break;
 				case 5:
 					$ra1 = $con->getUserById($token1->raId1);
 					$ra2 = $con->getUserById($token1->raId2);
 					$this->address = $ra1->email;
-					$this->subject = "Switch Request Notification";
+					$this->subject = "Switch Request Declined";
 					$this->content = "Dear " . $ra1->firstName . ",<br><br>";
-					$this->content = $ra2->firstName . " has declind your requested duty switch. <a href='" . SITE_URL . "login.php" . "'>Login</a> to view any accompanying messages.<br><br>";
-					$this->content .= "Thanks,<br>The ProjectSwitch Team";
+					$this->content .= $ra2->firstName . " has declined your requested duty switch. <a href='" . SITE_URL . "login.php" . "'>Login</a> to view any accompanying messages.<br><br>";
+					break;
+				case 6:
+					$rd = $con->getUserById($token->rdId);
+					$ra1 = $con->getUserById($token1->raId1);
+					$ra2 = $con->getUserById($token1->raId2);
+					$this->address = $rdId->email;
+					$this->subject = "Switch Request Notification";
+					$this->content = "Dear " . $rd->firstName . ",<br><br>";
+					$this->content .= $ra1->firstName . " and " . $ra2->firstName . " have confirmed a duty switch request: <a href='" . SITE_URL . "token.php?token=" . $token1->token . "'>APPROVE</a>  <a href='" . SITE_URL . "token.php?token=" . $token2->token . ">DENY</a><br><br>";
+					$thic->content .= "<a href='" . SITE_URL . "login.php" . "'>Login</a> to view specifics.<br><br>";
+					break;
+				case 7:
+					$rd = $con->getUserById($token->rdId);
+					$ra1 = $con->getUserById($token->raId1);
+					$this->address = $raId1->email;
+					$this->subject = "Switch Request Approved";
+					$this->content = "Dear " . $ra1->firstName . ",<br><br>";
+					$this->content .= $rd->firstName . " has approved your requested switch.<br><br>";
+					break;
+				case 8:
+					$rd = $con->getUserById($token->rdId);
+					$ra1 = $con->getUserById($token->raId1);
+					$this->address = $raId1->email;
+					$this->subject = "Switch Request Denied";
+					$this->content = "Dear " . $ra1->firstName . ",<br><br>";
+					$this->content .= $rd->firstName . " has denied your requested switch. <a href='" . SITE_URL . "login.php" . "'>Login</a> to view specifics.<br><br>";
+					break;
+				case 9:
+					$ra1 = $con->getUserById($token->raId1);
+					$this->address = $ra1->email;
+					$this->subject = "Project Switch Password Recovery";
+					$this->content = "Dear " . $ra1->firstName . ",<br><br>"
+					$this->content .= "A password recovery request has been intitiated for you.  Please click <a href='" . SITE_URL . "toekn.php?token=" . $ra1->token1 . "'>here</a> to complete the recovery process.<br><br>";
 					break;
 				default:
-					// Fatal error.  Shoot yourself in the foot and try again, dumbass.
+					$this->address = "mgrunert0322@gmail.com"
+					$this->subject = "SOMETHING'S WRONG IN THE MAILER"
+					$this->content = "You fucked up somewhere.  Go find it, dumbass.<br><br>"
 			}		
 
+			$this->content .= "Thanks,<br>The Project Switch Team";
 			$con->close();
 		}
 		
