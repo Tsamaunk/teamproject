852ad3fd096ddc79946796fe40440b9bf6802014
kisuk401 <kisuk401@hotmail.com>
2013-03-30 18:15:50 -0300

    Update calendar.php

--- calendar.php
+++ calendar.php
@@ -1,3 +1,32 @@
 <?php
+<?php
 
+include_once 'base.php';
+$hlp = new Helper();  	// CREATE THE HELPER
+$con = new Controller(); 	// AND A CONTROLLER
+
+
+if (isset($_SESSION['userId']) && isset($_SESSION['userToken']) && $hlp->validToken($_SESSION['userId'], $_SESSION['userToken'])) {
+	$myId = $_SESSION['userId'];
+} else {
+	header('Location: login.php');
+	exit;
+}
+
+// SOME HEADER HERE
+
+include_once 'header.php';
+include_once 'topbar.php'; ?>
+
+<div class="contaner-bottom">
+<?php include_once 'sidebar.php'; ?>
+	<div class="content">
+	<style>
+	td a {
+		margin-right: 10px;
+	}
+	</style>
+  
+  
+  
 ?>
